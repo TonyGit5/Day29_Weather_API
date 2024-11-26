9dae7763c73ae6224ed8e084b6e646fc7681b957
Tony DiMambro <TonyGit5@mail.com>
2024-11-26 13:46:32 -0500

    inital commit for WX_API project

--- flask_tutorial.py
+++ flask_tutorial.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import pandas as pd
 
 '''
 Notes
@@ -6,18 +7,25 @@ images need to be in the folder static
 html pages need to be in the folder templates
 '''
 
-app=Flask("Website")
+# best to use __name__ here - name takes on the value of the script executing
+# unless it's being run itslef then __name__ == __main__
+app=Flask("__name__")
 
 # http://127.0.0.1:5000/home
-@app.route("/home")
+@app.route("/")
 def home():
 
-    return render_template("flask_tutorial.html")
+    return render_template("home.html")
 
 # http://127.0.0.1:5000/about
-@app.route("/about/")
-def about():
-
+@app.route("/api/v1/<station>/<data>")
+def about(station, date):
+    df = pd.read_csv("")
+    temprature = df.station(date)
+    return {"sation": station,
+            "date": date,
+            "temprature": temprature}
     return render_template("about.html")
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
